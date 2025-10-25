7a35a98510a1d0fa0332ecdb8c6f10887b1af5f5
Pablo Hendrickx <pablo.hendrickx@atlascopco.com>
2025-10-25 20:41:12 +0200

    first intro

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  
 </script>
 
 <main>
-<div id="facecard">
+	<div id="facecard" class="flex items-center min-h-screen">
+		<div class="w-full max-w-150 mr-20 ml-20">
+			<enhanced:img
+				src="./assets/profile.jpeg"
+				alt="profile picture of an sre with his baby"
+				class="w-full h-full object-cover rounded-md"
+			/>
+		</div>
 
-</div>
+		<div>
+			<p class="text-3xl">Pablo Hendrickx</p>
+			<p class="text-2xl">Site Reliability Engineer</p>
+			<p>SRE with expertise in data systems, IoT and video streaming. 7 Years of experience.</p>
+		</div>
+	</div>
+	<div id="timeline" class="flex items-center min-h-screen">
+
+  </div>
 </main>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
+import { enhancedImages } from '@sveltejs/enhanced-img';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), svelte()],
+  plugins: [enhancedImages(),tailwindcss(), svelte()],
 });
