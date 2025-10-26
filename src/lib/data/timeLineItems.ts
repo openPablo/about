dd34e0b6b7a65e39197dbc9ab1ed5b5d150cb747
Pablo Hendrickx <pablo.hendrickx@atlascopco.com>
2025-10-26 15:07:29 +0100

    move timelineitems to a data file

---
+++ src/lib/data/timeLineItems.ts
@@ -0,0 +1,44 @@
+export const timeLineItems = [
+  {
+    date: "Aug 2024+",
+    employer: "Atlas Copco, Belgium",
+    title: "Site Reliability Engineer SmartLink",
+    notes: [
+      "SmartLink connects 350k+ heavy duty air compressors to an app which allows customers to see enriched information and proactively service their machines.",
+      "Led the SRE team, driving critical objectives, mentoring junior engineers, and prioritizing high-impact work.",
+      "Rolled out structured postmortems, streamlined incident management and response, trained on-call engineers.",
+      "Spearheaded ISO 27001 certification: led audit interviews, developed/tested the disaster recovery plan, gathered compliance evidence, and established user role and secure development policies.",
+      "Implemented OpenTelemetry and monitoring tool to enhance application performance monitoring.",
+    ],
+  },
+  {
+    date: "Jan 2022",
+    employer: "DPG Media, Belgium",
+    title: "Site Reliability Engineer, Streaming Area",
+    notes: [
+      "SRE for streaming platforms streamz.be and vtmgo.be which have over a million monthly users.",
+      "Incident response and management.",
+      "Led AWS and Akamai cost-saving initiatives, achieving a 30% reduction for my teams.",
+      "Created a custom Terraform provider in Go for internal APIs.",
+      "Built and managed an observability stack which was used by 10+ dev teams.",
+    ],
+  },
+  {
+    date: "March 2021",
+    employer: "Nubera, Belgium",
+    title: "DevOps Engineer Consultant, Keytrade bank",
+    notes: [
+      "Created a self-service platform using the GitLab ecosystem (CI/CD, AWS, Terraform modules).",
+      "Maintained Kubernetes clusters, Helm charts, infra-as-code, CI/CD.",
+    ],
+  },
+  {
+    date: "Feb 2018",
+    employer: "Cronos groep, Belgium",
+    title: "PostgreSQL DBA",
+    notes: [
+      "Created high-available on-premise PostgreSQL setups.",
+      "Oracle to PostgreSQL migrations to enable massive cost savings.",
+    ],
+  },
+];
